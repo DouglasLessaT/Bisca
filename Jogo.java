960faba075603ca2f61119884b2abe7679e08e5e
gabieluffy <131404044+gabieluffy@users.noreply.github.com>
2023-05-02 21:21:29 -0300

    Add files via upload
    Classe jogo - método jogadores

---
+++ Jogo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Jogo {
+    private List<Jogador> jogadores;
+    
+    public Jogo(List<String> nomesJogadores) {
+        jogadores = new ArrayList<>();
+        for (String nome : nomesJogadores) {
+           
+    }
+    }
+}
+
