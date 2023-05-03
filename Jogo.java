c7ed5b6ad88889fc1dd9a40afe1d6f67180bbbb2
gabieluffy <131404044+gabieluffy@users.noreply.github.com>
2023-05-02 21:29:25 -0300

    Add files via upload
    Adicionando o método embaralhar na classe jogo

--- Jogo.java
+++ Jogo.java
@@ -3,13 +3,33 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Jogo {
-    private List<Jogador> jogadores;
-    
-    public Jogo(List<String> nomesJogadores) {
-        jogadores = new ArrayList<>();
-        for (String nome : nomesJogadores) {
-           
-    }
-    }
-}
+	    private List<Jogador> jogadores;
+	    private Baralho baralho;
 
+	    public Jogo(List<String> nomesJogadores) {
+	        jogadores = new ArrayList<>();
+	        for (String nome : nomesJogadores) {
+	            jogadores.add(new Jogador(nome));
+	        }
+	        
+	        baralho = new Baralho();
+	    }
+	   
+	    public void jogar() {
+	        baralho.embaralhar();
+
+	        for (Jogador jogador : jogadores) {
+	            jogador.receberCarta(baralho.comprarCarta());
+	        }
+
+	        for (int i = 0; i < 4; i++) {
+	            List<Carta> cartasRodada = new ArrayList<>();
+	            for (Jogador jogador : jogadores) {
+	                Carta carta = jogador.jogarCarta(i);
+	                if (carta != null) {
+	                    cartasRodada.add(carta);
+	                }
+	            }
+	        }
+	       
+	          
