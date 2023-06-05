38c4ea639b390df7bc362175fd16333ad7622393
Michelle <109476419+ChellySil@users.noreply.github.com>
2023-06-04 23:11:50 -0300

    Update Jogador.java

--- src/entities/Jogador.java
+++ src/entities/Jogador.java
@@ -4,48 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Jogador {
-	
-	private String nome;
-    private List<Carta> cartasMao;
-    
-    
-    public Jogador(String nome, List<Carta> cartaMao) {
-        this.nome = nome;
-        cartasMao = new ArrayList<>(2);
-    }
-    
-    
-    public void setCartasMao(List<Carta> cartasMao) {
-		this.cartasMao = cartasMao;
+
+	    private String nome;
+	    private List<Carta> mao;
+
+	    public Jogador(String nome) {
+	        this.nome = nome;
+	        this.mao = new ArrayList<>();
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public List<Carta> getMao() {
+	        return mao;
+	    }
+
+	    public void adicionarCarta(Carta carta) {
+	        mao.add(carta);
+	    }
+
+	    public void removerCarta(Carta carta) {
+	        mao.remove(carta);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return nome;
+	    }
 	}
-    
-    
-    public void receberCartas (Carta carta) {
-		cartasMao.add(carta);
-    }
-    
-    
-    public Carta jogarCartas(List<Carta> cartasmesa, String Trunfo) {
-    	return cartasMao.remove((int)(Math.random()*cartasMao.size()));
-    }
-    
-    
-    public int contarPontos() {
-        int pontos = 0;
-        for (Carta carta : cartasMao) {
-            pontos += carta.getValor();
-        }
-        return pontos;
-    }
-    
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-		sb.append("Jogador: ").append(nome).append("\n");
-        sb.append("Cartas na mão: \n");
-        for (Carta carta : cartasMao) {
-            sb.append(carta.toString()).append("\n");
-        }
-        return sb.toString();
-    }
-}
+
