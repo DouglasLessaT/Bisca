6667411dfbedaebf8c8109436ab67abaa7b540a3
Michelle <109476419+ChellySil@users.noreply.github.com>
2023-06-04 23:13:40 -0300

    Update Main.java

--- src/entities/Main.java
+++ src/entities/Main.java
@@ -1,14 +1,27 @@
 package entities;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> nomesJogadores = Arrays.asList("Jogador 1", "Jogador 2", "Jogador 3", "Jogador 4");
-        Jogo jogo = new Jogo(nomesJogadores);
-        jogo.jogar();
-    }
-}
+	        Jogo  jogo  = new Jogo();
 
-   
+	        Jogador jogador1 = new Jogador("Jogador 1");
+	        Jogador jogador2 = new Jogador("Jogador 2");
+	        Jogador jogador3 = new Jogador("Jogador 3");
+	        Jogador jogador4 = new Jogador("Jogador 4");
+
+
+	        jogo.adicionarJogador(jogador1);
+	        jogo.adicionarJogador(jogador2);
+	        jogo.adicionarJogador(jogador3);
+	        jogo.adicionarJogador(jogador4);
+
+	        jogo.iniciar();
+	    }
+
+
+	}
+
