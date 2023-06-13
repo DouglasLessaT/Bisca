a58924ffe2b7c3f84264305fef1279c66ae25c90
Michelle <109476419+ChellySil@users.noreply.github.com>
2023-06-12 21:44:44 -0300

    Add files via upload

---
+++ src/main/java/Baralho.java
@@ -0,0 +1,40 @@
+package java;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Baralho {
+	 private List<Carta> cartas;
+
+	    public Baralho() {
+	        cartas = new ArrayList<>();
+
+	        for (Naipe naipe : Naipe.values()) {
+	            for (Valor valor : Valor.values()) {
+	                cartas.add(new Carta(naipe, valor));
+	            }
+	        }
+	    }
+
+	    public void embaralhar() {
+	        Random random = new Random();
+
+	        for (int i = cartas.size() - 1; i > 0; i--) {
+	            int j = random.nextInt(i + 1);
+	            Carta temp = cartas.get(i);
+	            cartas.set(i, cartas.get(j));
+	            cartas.set(j, temp);
+	        }
+	    }
+
+	    public Carta distribuirCarta() {
+	        return cartas.remove(cartas.size() - 1);
+	    }
+
+	    public boolean isEmpty() {
+	        return cartas.isEmpty();
+	    }
+	}
+
+
---
+++ src/main/java/Jogador.java
@@ -0,0 +1,37 @@
+package java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+
+	    private String nome;
+	    private List<Carta> mao;
+
+	    public Jogador(String nome) {
+	        this.nome = nome;
+	        this.mao = new ArrayList<>();
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public List<Carta> getMao() {
+	        return mao;
+	    }
+
+	    public void adicionarCarta(Carta carta) {
+	        mao.add(carta);
+	    }
+
+	    public void removerCarta(Carta carta) {
+	        mao.remove(carta);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return nome;
+	    }
+	}
+
